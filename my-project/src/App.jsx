40c7c55f52de5028e28cbe1648767c34553954b6
anaskaysar <kaysarul.apurba@northsouth.edu>
2024-11-11 20:36:46 -0500

    Slightest Change

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,29 +1,11 @@
-import { useState } from 'react'
-
 import './App.css'
-import Header from './components/Header'
-import AlgorithmSelector from './components/AlgorithmSelector'
-import InputSection from './components/InputSection'
-import Header2 from './components/Header2'
+import Home from './pages/Home'
 
 function App() {
-  const [algorithm, setAlgorithm] = useState('AES');
-  const [input, setInput] = useState('');
-
-  const handleInputChange = (value) => setInput(value);
 
   return (
     <>
-      <div className="App">
-      <Header />
-      {/* <Header2/> */}
-      <AlgorithmSelector setAlgorithm={setAlgorithm} />
-      <InputSection handleInputChange={handleInputChange} />
-      <div className="text-center my-4">
-        <p>Selected Algorithm: {algorithm}</p>
-        <p>Input: {input}</p>
-      </div>
-    </div>
+     <Home/>
     </>
   )
 }
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import { useState } from 'react'
+
+import Header from "../components/Header";
+import InputSection from "../components/InputSection";
+import AlgorithmSelector from "../components/AlgorithmSelector";
 
 const Home = () => {
-    return (
-        <div>
-            
+  const [algorithm, setAlgorithm] = useState("AES");
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (value) => setInput(value);
+  return (
+    <>
+      <div>
+        <Header />
+        <AlgorithmSelector setAlgorithm={setAlgorithm}  />
+        <InputSection handleInputChange={handleInputChange} />
+        <div className="text-center my-4">
+          <p>Selected Algorithm: {algorithm} </p>
+          <p>Input: {input} </p>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Home;
